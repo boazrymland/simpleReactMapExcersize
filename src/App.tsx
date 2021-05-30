98b5136e4036ff26c4fcc0e541017e2fe5a9008b
BoazR <boazr@tikalk.com>
2021-05-30 22:36:07 +0300

    initial version, clean and doing not a lot

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Chart} from 'react-google-charts';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header>
+                This is the header area
+            </header>
+            <section>
+                This is the main content area
+            </section>
+        </div>
+    );
 }
 
 export default App;
